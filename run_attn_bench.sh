6caf4bab55366534710261b965e2c8a9a5d448ff
Alexey Kuts <krunt@yandex.ru>
2022-02-21 22:58:20 +0300

    some bench observed

---
+++ run_attn_bench.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+seq_length=1024
+num_seqs_start=10
+num_seqs_stop=10
+layers=4
+hidden_dim=1024
+heads=16
+
+
+for attn_type in '' '--ref' '--native' '--lean' ; do 
+
+  python3 perf_test_multihead_attn.py --seq-length=$seq_length --num-seqs-start=$num_seqs_start --num-seqs-stop=$num_seqs_stop --layers=$layers --hidden-dim=$hidden_dim --heads=$heads --norm-add $attn_type
+
+done
